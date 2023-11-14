dfb74f3d1427305a53fe83ff1a7cdeb5259ea759
Canopusinfosystemspvt <dilipcanopus@gmail.com>
2023-11-14 12:59:59 +0530

    details page

--- src/pages/Formdata.jsx
+++ src/pages/Formdata.jsx
@@ -25,36 +25,38 @@ const Formdata = () => {
   return (
     <div>
       <h2 className='text-center pt-5 pb-3 mt-5'>Contact Form Data</h2>
-      <table className="contact-table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Date / Time</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Message</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.length === 0 ? (
+      <div className="table-container">
+        <table className="contact-table">
+          <thead>
             <tr>
-              <td colSpan="3">No data available</td>
+              <th>ID</th>
+              <th>Date / Time</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Message</th>
             </tr>
-          ) : (
-            data.map((item, index) => (
-              <tr key={index} className={index % 2 === 0 ? 'even-row' : 'odd-row'}>
-                <td>{item.id}</td>
-                <td>{item.submission_date}</td>
-                <td>{item.name}</td>
-                <td>{item.email}</td>
-                <td>{item.phone}</td>
-                <td>{item.message}</td>
+          </thead>
+          <tbody>
+            {data.length === 0 ? (
+              <tr>
+                <td colSpan="3">No data available</td>
               </tr>
-            ))
-          )}
-        </tbody>
-      </table>
+            ) : (
+              data.map((item, index) => (
+                <tr key={index} className={index % 2 === 0 ? 'even-row' : 'odd-row'}>
+                  <td>{item.id}</td>
+                  <td>{item.submission_date}</td>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                  <td>{item.message}</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
