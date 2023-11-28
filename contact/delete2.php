9ed1ce7ea4d2221dad083f6a551a28e1bdcd88f1
Canopusinfosystemspvt <dilipcanopus@gmail.com>
2023-11-28 12:01:33 +0530

    2

--- contact/delete2.php
+++ contact/delete2.php
@@ -1,31 +1,48 @@
 <?php
-// Enable CORS (Cross-Origin Resource Sharing)
-header('Access-Control-Allow-Origin: https://swasthik-technology.vercel.app');
-header('Access-Control-Allow-Methods: DELETE');
-header('Access-Control-Allow-Headers: Content-Type');
 
-// Handle DELETE request
-if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-    // Database credentials
-    $servername = "localhost";
-    $username = "u171790274_contact_form";
-    $password = "Dilip@7878";
-    $dbname = "u171790274_contact_form";
+header("Access-Control-Allow-Origin: https://swasthik-technology.vercel.app");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
 
-    // Create a connection to the database
-    $conn = new mysqli($servername, $username, $password, $dbname);
+// Connect to the database (replace with your database credentials)
+$servername = "localhost";
+$username = "u171790274_contact_form";
+$password = "Dilip@7878";
+$dbname = "u171790274_contact_form";
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+	header("Access-Control-Allow-Origin: https://swasthik-technology.vercel.app");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
+	header("Access-Control-Allow-Headers: Content-Type");
+	header("Access-Control-Allow-Credentials: true");
+    // Handle GET request to retrieve data from the database
+    $sql = "SELECT * FROM contact_form";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $data = array();
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    } else {
+        echo "No records found in the database.";
     }
-
-    // Check if an ID parameter is passed in the request
+    $conn->close();
+} elseif ($_SERVER["REQUEST_METHOD"] === "DELETE") {
+    // Handle DELETE request to delete a record from the database
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
 
         // Prepare a DELETE SQL statement
-        $sql = "DELETE FROM your_table_name WHERE id = ?";
+        $sql = "DELETE FROM contact_form WHERE id = ?";
 
         // Create a prepared statement
         $stmt = $conn->prepare($sql);
@@ -69,12 +86,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
         );
         echo json_encode($response);
     }
-
-    // Close the database connection
-    $conn->close();
-} else {
-    // Invalid HTTP method
-    http_response_code(405); // Method Not Allowed
-    echo json_encode(array("message" => "Invalid request method."));
 }
 ?>
--- src/pages/Formdata.jsx
+++ src/pages/Formdata.jsx
@@ -38,25 +38,23 @@ const FormData = () => {
         <p>Loading...</p>
       ) : (
         <div className="table-container">
-          <table className="contact-table">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Date / Time</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Message</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.length === 0 ? (
+          {data.length === 0 ? (
+            <p>No data available</p>
+          ) : (
+            <table className="contact-table">
+              <thead>
                 <tr>
-                  <td colSpan="6">No data available</td>
+                  <th>ID</th>
+                  <th>Date / Time</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Message</th>
+                  <th>Action</th>
                 </tr>
-              ) : (
-                data.map((item) => (
+              </thead>
+              <tbody>
+                {data.map((item) => (
                   <tr key={item.id}>
                     <td>{item.id}</td>
                     <td>{item.submission_date}</td>
@@ -68,10 +66,10 @@ const FormData = () => {
                       <button onClick={() => handleDelete(item.id)}>Delete</button>
                     </td>
                   </tr>
-                ))
-              )}
-            </tbody>
-          </table>
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
       )}
     </div>
