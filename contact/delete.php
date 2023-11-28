3849f36edd14434351db533c1e3ed2017fbf0689
Canopusinfosystemspvt <dilipcanopus@gmail.com>
2023-11-28 11:02:31 +0530

    data deletion

--- contact/delete.php
+++ contact/delete.php
@@ -1,31 +1,79 @@
 <?php
+// Enable CORS (Cross-Origin Resource Sharing)
 header("Access-Control-Allow-Origin: https://swasthik-technology.vercel.app");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: DELETE");
+header("Content-Type: application/json");
 
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    // Database credentials
+    $servername = "localhost";
+    $username = "u171790274_contact_form";
+    $password = "Dilip@7878";
+    $dbname = "u171790274_contact_form";
 
-$pdo = new PDO('mysql:host=localhost;dbname=u171790274_contact_form', 'u171790274_contact_form', 'Dilip@7878');
+    // Create a connection to the database
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Retrieve ID from request
-$id = $_GET['id'] ?? null; // Or use $_POST if you're sending data in the body
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-// Check if ID is provided
-if (!$id) {
-    echo json_encode(['error' => 'Missing ID']);
-    exit;
-}
+    // Check if an ID parameter is passed in the request
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
 
-// Prepare SQL query
-$stmt = $pdo->prepare("DELETE FROM your_table WHERE id = :id");
+        // Prepare a DELETE SQL statement
+        $sql = "DELETE FROM your_table_name WHERE id = ?";
 
-// Bind parameters and execute
-$stmt->execute(['id' => $id]);
+        // Create a prepared statement
+        $stmt = $conn->prepare($sql);
 
-// Check if delete was successful
-if ($stmt->rowCount()) {
-    echo json_encode(['success' => 'Record deleted']);
+        if ($stmt) {
+            // Bind the ID parameter to the statement
+            $stmt->bind_param("i", $id);
+
+            // Execute the DELETE statement
+            if ($stmt->execute()) {
+                // Record deleted successfully
+                $response = array(
+                    "status" => "success",
+                    "message" => "Record deleted successfully."
+                );
+                echo json_encode($response);
+            } else {
+                // Error while executing the DELETE statement
+                $response = array(
+                    "status" => "error",
+                    "message" => "Error deleting the record."
+                );
+                echo json_encode($response);
+            }
+
+            // Close the prepared statement
+            $stmt->close();
+        } else {
+            // Error in preparing the DELETE statement
+            $response = array(
+                "status" => "error",
+                "message" => "Error preparing the DELETE statement."
+            );
+            echo json_encode($response);
+        }
+    } else {
+        // ID parameter is missing in the request
+        $response = array(
+            "status" => "error",
+            "message" => "Missing 'id' parameter in the request."
+        );
+        echo json_encode($response);
+    }
+
+    // Close the database connection
+    $conn->close();
 } else {
-    echo json_encode(['error' => 'Record not found or not deleted']);
+    // Invalid HTTP method
+    http_response_code(405); // Method Not Allowed
+    echo json_encode(array("message" => "Invalid request method."));
 }
 ?>
--- src/pages/Formdata.jsx
+++ src/pages/Formdata.jsx
@@ -1,34 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Formdata = () => {
+const FormData = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    document.title = 'Details | Swasthik Technology';
-    window.scrollTo(0, 0);
-  }, []);
-
   const fetchData = () => {
     axios.get('https://apoorvmathur.online/contact/form-details.php')
       .then((response) => {
-        if (Array.isArray(response.data)) {
-          setData(response.data);
-        } else {
-          setData([]);
-        }
+        setData(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
+        setLoading(false);
       });
   };
 
   const handleDelete = (id) => {
-    axios.get(`https://apoorvmathur.online/contact/delete.php?id=${id}`)
+    axios.delete(`https://apoorvmathur.online/contact/delete.php?id=${id}`)
       .then(() => {
         fetchData(); // Reload data after delete
       })
@@ -40,44 +34,48 @@ const Formdata = () => {
   return (
     <div>
       <h2 className='text-center pt-5 pb-3 mt-5'>Contact Form Data</h2>
-      <div className="table-container">
-        <table className="contact-table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Date / Time</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Message</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.length === 0 ? (
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="table-container">
+          <table className="contact-table">
+            <thead>
               <tr>
-                <td colSpan="6">No data available</td>
+                <th>ID</th>
+                <th>Date / Time</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Message</th>
+                <th>Action</th>
               </tr>
-            ) : (
-              data.map((item, index) => (
-                <tr key={index} className={index % 2 === 0 ? 'even-row' : 'odd-row'}>
-                  <td>{item.id}</td>
-                  <td>{item.submission_date}</td>
-                  <td>{item.name}</td>
-                  <td>{item.email}</td>
-                  <td>{item.phone}</td>
-                  <td>{item.message}</td>
-                  <td>
-                    <button onClick={() => handleDelete(item.id)}>Delete</button>
-                  </td>
+            </thead>
+            <tbody>
+              {data.length === 0 ? (
+                <tr>
+                  <td colSpan="6">No data available</td>
                 </tr>
-              ))
-            )}
-          </tbody>
-        </table>
-      </div>
+              ) : (
+                data.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.submission_date}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td>{item.message}</td>
+                    <td>
+                      <button onClick={() => handleDelete(item.id)}>Delete</button>
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
 
-export default Formdata;
+export default FormData;
